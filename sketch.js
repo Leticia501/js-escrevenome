1fe4ddbfdb833335abbabaa8073edbcb179be3e4
lele <00001125052806SP@al.educacao.sp.gov.br>
2024-09-03 10:28:45 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,44 @@
+// viagem ao centro da terra, LIVRE, acao, aventura, familia, fantasia, ficcao cientifica
+// divertida mente 2, LIVRE, animacao, aventura, familia
+// meu malvado favorito 4, 10, animacao, aventura, comedia
+// a família addams, 12, comedia, fantasia
+// star wars, LIVRE, acao, aventura, fantasia, ficcao cientifica
+
+
+let campoIdade;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+}
+
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
